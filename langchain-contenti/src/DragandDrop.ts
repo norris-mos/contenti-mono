2e75428d9c3684f7330d8eca2a56dd1371d64b13
norris-mos <maxonorris@gmail.com>
2023-07-15 20:42:46 +0100

    before promptedit

---
+++ langchain-contenti/src/DragandDrop.ts
@@ -0,0 +1,25 @@
+import { LLMChain } from 'langchain';
+import { PromptTemplate } from 'langchain/prompts';
+import { buildModel } from './index';
+
+export default function DragandDrop(inputPrompt: string) {
+  const model = buildModel();
+
+  const template =
+    'You are a {grade} grade teacher writing a {excercise} on {Topic}. Come up with some potential examples';
+
+  const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ['grade', 'excercise', 'Topic'],
+  });
+
+  const chain = new LLMChain({ llm: model, prompt: prompt });
+
+  const res = await chain.call({
+    grade: 'fourth',
+    excercise: 'MCQ',
+    Topic: 'berries',
+  });
+
+  console.log(res);
+}
